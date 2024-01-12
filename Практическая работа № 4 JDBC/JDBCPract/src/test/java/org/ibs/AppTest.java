feaedabf4e26b3abcc6f1aa0697f1a0329dfe5bb
Ilyas <ilyas_mavlyutov@bk.ru>
2024-01-12 17:08:09 +0400

    ПР № 4

--- Практическая работа № 4 JDBC/JDBCPract/src/test/java/org/ibs/AppTest.java
+++ Практическая работа № 4 JDBC/JDBCPract/src/test/java/org/ibs/AppTest.java
@@ -1,2 +1,69 @@
-package org.ibs;public class AppTest {
+package org.ibs;
+
+import org.junit.jupiter.api.Test;
+import org.ibs.basetests.JDBCManager;
+import java.sql.*;
+
+
+
+public class AppTest extends JDBCManager{
+
+    @Test
+    public void testCase1() throws SQLException {
+        getStatement();
+        insertInvalidValues(5,null,null,12);
+        deleteById(5);
+    }
+
+    @Test
+    public void testCase2() throws SQLException {
+        getStatement();
+        insertInvalidValues(1,"Яблоко", "FRUIT", 0);
+        deleteById(5);
+    }
+    @Test
+    public void testCase3() throws SQLException {
+        getStatement();
+        insertInvalidValues(1, "Банан", "FRUIT", 12);
+        deleteById(1);
+    }
+
+    @Test
+    public void testCase4() throws SQLException {
+        getStatement();
+        insertInvalidValues(1, "Банан", null, 0);
+        deleteById(1);
+    }
+
+    @Test
+    public void testCase5() throws SQLException {
+        getStatement();
+        insertInvalidValues(5, "Банан", null, 0);
+        deleteById(5);
+    }
+
+    @Test
+    public void testCase6() throws SQLException {
+        getStatement();
+        insertInvalidValues(5, "Банан", "FRUIT", 12);
+        deleteById(5);
+    }
+    @Test
+    public void testCase7() throws SQLException {
+        getStatement();
+        insertInvalidValues(5, null, "FRUIT", 0);
+        deleteById(5);
+    }
+    @Test
+    public void testCase8() throws SQLException {
+        getStatement();
+        insertInvalidValues(1, null, null, 12);
+        deleteById(1);
+    }
+    @Test
+    public void testCase9() throws SQLException {
+        getStatement();
+        insertIntoFood(5, "Банан", "FRUIT", 0);
+        deleteById(5);
+    }
 }
--- Практическая работа № 4 JDBC/JDBCPract/src/test/java/org/ibs/basetests/JDBCManager.java
+++ Практическая работа № 4 JDBC/JDBCPract/src/test/java/org/ibs/basetests/JDBCManager.java
@@ -1,11 +1,17 @@
-package org.ibs;
+package org.ibs.basetests;
 
 import org.h2.jdbc.JdbcSQLDataException;
 import org.h2.jdbc.JdbcSQLIntegrityConstraintViolationException;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 import java.sql.*;
 
 public class JDBCManager {
+
+    public static int countTests = 1;
     public static final Connection connection;
 
     static {
@@ -19,18 +25,37 @@ public class JDBCManager {
         return connection.createStatement();
     }
 
-    public void insertIntoFood(Integer foodId, String foodName, String foodType, Integer foodExotic) throws SQLException{
+    /**
+     * Метод выполняет запрос для добавления продукта в БД
+     * @param foodId ID продукта
+     * @param foodName наименование продукта
+     * @param foodType тип продукта
+     * @param foodExotic экзотичность продукта
+     */
+    public void insertIntoFood(Integer foodId, String foodName, String foodType, Integer foodExotic){
         String insert = "insert into food(food_id, food_name, food_type, food_exotic) values (?, ?, ?, ?);";
-
+        try {
             PreparedStatement preparedStatement = connection.prepareStatement(insert);
             preparedStatement.setInt(1, foodId);
             preparedStatement.setString(2, foodName);
             preparedStatement.setString(3,foodType);
             preparedStatement.setInt(4,foodExotic);
             preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Запись добавлена в БД");
 
     }
 
+    /**
+     * Метод выполняет запрос с некорректными данными для попытки добавления продукта в БД
+     * @param foodId ID продукта
+     * @param foodName наименование продукта
+     * @param foodType тип продукта
+     * @param foodExotic экзотичность продукта
+     */
     public void insertInvalidValues(Integer foodId, String foodName, String foodType, Integer foodExotic) throws SQLException{
         String insert = "insert into food(food_id, food_name, food_type, food_exotic) values (?, ?, ?, ?);";
         try {
@@ -47,14 +72,37 @@ public class JDBCManager {
 
     }
 
+    /**
+     * Удаляет продукт из БД по FOOD_ID
+     * @param foodId ID продукта
+     */
     public void deleteById(Integer foodId) {
-        String delete = "delete from FOOD where food_id = ?;";
+        String delete = "delete from FOOD where food_id = " + foodId;
         try {
-            PreparedStatement preparedStatement = connection.prepareStatement(delete);
-            preparedStatement.setInt(1,foodId);
+            getStatement().execute(delete);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("Запуск тестов...");
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        System.out.println("Запуск теста №" + countTests);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        countTests++;
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("Все тесты завершены");
     }
 
 
