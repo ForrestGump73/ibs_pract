c27b9b4667fe13c65e6b120829a1c9c9aca23738
Ilyas <ilyas_mavlyutov@bk.ru>
2024-01-15 21:52:35 +0400

    allure

--- Практическая работа № 5 Cucumber/allure_cucumber/src/test/java/org/ibs/jdbc/basetests/NullableConverter.java
+++ Практическая работа № 5 Cucumber/allure_cucumber/src/test/java/org/ibs/jdbc/basetests/NullableConverter.java
@@ -1,2 +1,15 @@
-package org.ibs.jdbc.basetests;public class NullableConverter {
+package org.ibs.jdbc.basetests;
+
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.DefaultArgumentConverter;
+import org.junit.jupiter.params.converter.SimpleArgumentConverter;
+
+public final class NullableConverter extends SimpleArgumentConverter {
+    @Override
+    protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
+        if ("null".equals(source)) {
+            return null;
+        }
+        return DefaultArgumentConverter.INSTANCE.convert(source, targetType);
+    }
 }
