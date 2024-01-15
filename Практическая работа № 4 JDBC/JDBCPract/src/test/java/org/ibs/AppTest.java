56ed03535568702035e7e5ea77ba907bd0b3d163
Ilyas <ilyas_mavlyutov@bk.ru>
2024-01-15 13:10:20 +0400

    Добавил все тесты в 1 параметризированный тест

--- Практическая работа № 4 JDBC/JDBCPract/src/test/java/org/ibs/AppTest.java
+++ Практическая работа № 4 JDBC/JDBCPract/src/test/java/org/ibs/AppTest.java
@@ -1,69 +1,44 @@
 package org.ibs;
 
+import org.ibs.basetests.NullableConverter;
 import org.junit.jupiter.api.Test;
 import org.ibs.basetests.JDBCManager;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import java.sql.*;
 
 
 
 public class AppTest extends JDBCManager{
 
-    @Test
-    public void testCase1() throws SQLException {
+
+    @ParameterizedTest
+    @CsvSource({
+        "5, null, null,12",
+        "1, Яблоко, FRUIT, 0",
+        "1, Банан, FRUIT, 12",
+        "1, Банан, null, 0",
+        "5, Банан, null, 0",
+        "5, Банан, FRUIT, 12",
+        "5, null, FRUIT, 0",
+        "1, null, null, 12"
+    } )
+    public void testInvalidValues(int food_id,
+                                      @ConvertWith(NullableConverter.class) String food_name,
+                                      @ConvertWith(NullableConverter.class) String food_type,
+                                      int food_exotic)
+            throws SQLException{
         getStatement();
-        insertInvalidValues(5,null,null,12);
-        deleteById(5);
+        insertInvalidValues(food_id,food_name,food_type,food_exotic);
     }
 
     @Test
-    public void testCase2() throws SQLException {
-        getStatement();
-        insertInvalidValues(1,"Яблоко", "FRUIT", 0);
-        deleteById(5);
-    }
-    @Test
-    public void testCase3() throws SQLException {
-        getStatement();
-        insertInvalidValues(1, "Банан", "FRUIT", 12);
-        deleteById(1);
-    }
-
-    @Test
-    public void testCase4() throws SQLException {
-        getStatement();
-        insertInvalidValues(1, "Банан", null, 0);
-        deleteById(1);
-    }
-
-    @Test
-    public void testCase5() throws SQLException {
-        getStatement();
-        insertInvalidValues(5, "Банан", null, 0);
-        deleteById(5);
-    }
-
-    @Test
-    public void testCase6() throws SQLException {
-        getStatement();
-        insertInvalidValues(5, "Банан", "FRUIT", 12);
-        deleteById(5);
-    }
-    @Test
-    public void testCase7() throws SQLException {
-        getStatement();
-        insertInvalidValues(5, null, "FRUIT", 0);
-        deleteById(5);
-    }
-    @Test
-    public void testCase8() throws SQLException {
-        getStatement();
-        insertInvalidValues(1, null, null, 12);
-        deleteById(1);
-    }
-    @Test
-    public void testCase9() throws SQLException {
+    public void testValidValues() throws SQLException {
         getStatement();
         insertIntoFood(5, "Банан", "FRUIT", 0);
         deleteById(5);
     }
+
 }
